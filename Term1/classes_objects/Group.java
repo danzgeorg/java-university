5d7c4ae34d196d3cbf23fe85cc1a77a4a3faea3d
daniel georgiev <danzgeorg.contact@gmail.com>
2025-02-18 14:12:44 +0000

    Add files via upload

---
+++ Term1/classes_objects/Group.java
@@ -0,0 +1,13 @@
+package StudentTask;
+
+import java.util.ArrayList;
+
+public class Group {
+    Student student1;
+    Student student2;
+
+    public Group(Student student1, Student student2) {
+        this.student1 = student1;
+        this.student2 = student2;
+    }
+}
